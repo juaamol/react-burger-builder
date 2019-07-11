106200b528584282875a48e66aa081b31bda48a7
juaamol <jambulud@gmail.com>
2019-07-11 20:05:43 +0200

    fix cheese add more: chesese was instead of cheese

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -5,7 +5,7 @@ import BuildControl from './BuildControl/BuildControl';
 const controls = [
     { label: 'Salad', type: 'salad' },
     { label: 'Bacon', type: 'bacon' },
-    { label: 'Cheese', type: 'chesese' },
+    { label: 'Cheese', type: 'cheese' },
     { label: 'Meat', type: 'meat' },
 ];
 
