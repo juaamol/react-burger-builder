f45a17feae4a640c5546fa09581854c90058fac1
juaamol <jambulud@gmail.com>
2019-08-31 20:17:35 +0200

    checkout build using query params

--- src/containers/BurgerBuilder/BuilderBuilder.js
+++ src/containers/BurgerBuilder/BuilderBuilder.js
@@ -66,7 +66,21 @@ class BurgerBuilder extends Component {
     //       .catch(err => {
     //         this.setState({ loading: false, purchasing: false });
     //       });
-    this.props.history.push('/checkout');
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          '=' +
+          encodeURIComponent(this.state.ingredients[i]),
+      );
+    }
+
+    const queryStr = queryParams.join('&');
+
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryStr,
+    });
   };
 
   updatePurchaseState = updatedIngredients => {
